058799184480841c3f4e0ae31fbff2fbc4206363
George Lestaris <glestaris@gmail.com>
2017-04-05 19:36:09 +0000

    Add initial protos
    ...and infrastructure to process/compile protos.

---
+++ src/web/DatabaseClient.php
@@ -0,0 +1,32 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace  {
+
+  class DatabaseClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+      parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \TodoItem $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function CreateTodoItem(\TodoItem $argument,
+      $metadata = [], $options = []) {
+      return $this->_simpleRequest('/Database/CreateTodoItem',
+      $argument,
+      ['\TodoItem', 'decode'],
+      $metadata, $options);
+    }
+
+  }
+
+}
